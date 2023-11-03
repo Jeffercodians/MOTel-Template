ce107b6df766e5ee6b95dfc7e01bbd90786196b9
ElectronicPanopticon <gaoler@electronicpanopticon.com>
2023-11-02 18:09:14 -0700

    Added JUnit Jupiter sceleton

---
+++ src/test/java/com/jeffercodians/motel/ExampleModTest.java
@@ -0,0 +1,11 @@
+package com.jeffercodians.motel;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ExampleModTest {
+    @Test
+    public void testExampleMod() {
+        assert true;
+    }
+}
